f4fa42a89c14e5f670a989a27bbf1b9c5adca2dc
Nick <nick.moi@gmail.com>
2019-12-18 16:30:56 -0500

    Done.

---
+++ src/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+class Message extends React.Component {
+  render() {
+    return (<div>
+        <h1>These are your messages</h1>
+        <p>Message 1</p>
+        <p>Message 2</p>
+    </div>)
+  }
+}
+ 
+export default Message;
